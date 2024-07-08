a49ab9ea41a8006cde4d859c638f710710e0e9ad
Beaprofessor <professornberlin2122@gmail.com>
2024-07-08 14:17:59 +0530

    Learned how to kickstart the react project.

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai aur React Series | </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+<h1>Chai aur React  with Vite | Lokesh Sharma |
+   <h6>Discovered two methods to start React projects in 'Chai aur React' series 2nd video:
+   1. Use npx create-react-app project_name.
+   2. Try Vite bundler npm run vite@latest` for quick setup. 
+   
+   Also, learned the importance of confidently cleaning up unused files to enhance learning! </h6>
+</h1>
+  )
+}
+
+export default App
