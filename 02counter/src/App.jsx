221fd94c30ebc2848308a9eed05c5229cd380c3b
Beaprofessor <professornberlin2122@gmail.com>
2024-07-09 16:12:39 +0530

    Interview ques on Counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -13,6 +13,23 @@ function App() {
     // counter = counter + 1;
     if(counter < 20){
       setCounter(counter + 1)
+
+/*
+      // Ye ab saare ek jaisa hi kaam kar rahe hai toh yaha par diff algorithm of react wo dekhega ki yaha toh sab kuch same hi toh wo inn sabko ek hi batch mei bhejega and ek baar hi kaam karega..
+      setCounter(counter + 1)
+      setCounter(counter + 1)
+      setCounter(counter + 1)   
+      setCounter(counter + 1)  
+
+      
+       Yahaa ab isme change hoga kyuki yaha par wo har baar previous value lekar usme ek nayi value add kar raha hai .( kyuki yaha ek callback milta  hai (jo bhi last updated state hai uski toh wo uske andar value add karega.), like promise yaad hai na ek this hua fir next this , fir next this..... ) (kyuki callback se aata hai and uske baad ek call hoti hai and fir waha new value add)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+
+      */
+
     }
   }
 
