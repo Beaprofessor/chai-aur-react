1e703a269f17a5d2410aad3f99152d3433da3d94
Beaprofessor <professornberlin2122@gmail.com>
2024-08-17 15:39:22 +0530

    Production grade react components

---
+++ 12MegaBlog/src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Button({
+    btnText,
+    type = 'button',
+    bgColor = 'bg-blue-600',
+    textColor = 'text-white',
+    className = '',
+    ...props
+}) {
+    return (
+            <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+                {btnText}
+            </button>
+    )
+}
+
+export default Button
