d974123dcf507c2621aaebbaedcc4a84907b1740
Beaprofessor <professornberlin2122@gmail.com>
2024-08-19 21:18:12 +0530

    Adding form and slug values

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,6 +3,9 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Select from "./Select"
+
 
 export {
     Header,
